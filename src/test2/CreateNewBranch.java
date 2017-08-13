3c51470bdb1751b50d655fc0b4fd7f6e2780d01e
home_pc <cngdsch@163.com>
2017-08-14 00:58:36 +0800

    修改int a = 1;

--- src/test2/CreateNewBranch.java
+++ src/test2/CreateNewBranch.java
@@ -4,7 +4,7 @@ public class CreateNewBranch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 1;
 		System.out.println("CreateNewBranch222");
 
 		System.out.println("CreateNewBranch111");
